df5f5d9b4e12991b6e6a2880d7e3c1846ed1f39d
Spinnafre <72418810+DaviSpin@users.noreply.github.com>
2023-04-04 14:25:17 -0300

    feature :get values from average parameters from stations

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import { setTimeout } from "node:timers/promises";
 import Joi from "joi";
 
 import { createWriteStream } from "node:fs";
-import { Readable, Transform } from "node:stream";
-
-import expect from "node:assert";
+import { Readable, Transform, Writable } from "node:stream";
+import { pipeline } from "node:stream/promises";
 
 import { dataAsStream } from "./server_utils.js";
 
@@ -29,7 +28,7 @@ app.use(
 );
 
 app.get("/test", async (req, res) => {
-  const { country, type } = req.query;
+  const { country, type, state } = req.query;
 
   const schema = Joi.object({
     country: Joi.string()
@@ -45,6 +44,8 @@ app.get("/test", async (req, res) => {
       country,
       type,
     });
+
+    // CONFIGURAÇÃO DE ACESSO A PÁGINA
 
     const browser = await puppeteer.launch({
       headless: true,
@@ -91,6 +92,7 @@ app.get("/test", async (req, res) => {
     });
 
     await setTimeout(1000);
+    // FIM DE CONFIGURAÇÃO DE ACESSO A PÁGINA
 
     //Selecionar região = [BRASIL,N,NE,CO,SE,S]
     const selectStationRegionBtn = await page.$("#estacao-regiao");
@@ -100,78 +102,175 @@ app.get("/test", async (req, res) => {
     const selectStationTypeBtn = await page.$("#estacao-tipo");
     await selectStationTypeBtn.select(type);
 
+    //Selecionar tipo da estação (todas, automaticas, convencionais)
+    const selectDataTypeBtn = await page.$("#estacao-tipo-dados");
+    await selectDataTypeBtn.select("diario");
+
+    await setTimeout(300);
+
+    // Ver a possibilidade de fazer regex com os parâmetros da página
+    const defaultStationMeditionsParams = [
+      "Precipitação Total (mm)",
+      "Temp. Média (°C)",
+      "Umi. Média (%)",
+      "Vel. do Vento Média (m/s)",
+    ];
+
+    // const stationParamsSelect = await page.$("#estacao-parametro");
+
+    const allStationsParams = await page.evaluate(() => {
+      const stationParamsOptions =
+        document.querySelector("#estacao-parametro").children;
+
+      const stationParamsSanitized = Array.from(stationParamsOptions).map(
+        (option) => ({
+          label: option.innerHTML,
+          value: option.value,
+        })
+      );
+      return stationParamsSanitized;
+    });
+
+    const stationParamsLabelsToQuery = allStationsParams
+      .filter((params) => {
+        return defaultStationMeditionsParams.includes(params.label);
+      })
+      .map((params) => params.value);
+
+    console.log(stationParamsLabelsToQuery);
+
+    if (!stationParamsLabelsToQuery) {
+      await page.close();
+
+      await browser.close();
+      throw new Error(
+        "Não foi possível obter identificadores dos parâmetros das medições das estações"
+      );
+    }
+
     await page.waitForSelector(".btn-green");
 
-    const data = await page.$eval("#btn-estacao-BUSCAR", (el) => {
-      el.click();
-      return el.textContent.trim();
-    });
-    console.log("Clicando no botão : ", data);
+    const datas = new Map();
 
-    //Promise para esperar pela a resposta da requisição AJAX
-    //Que não seja a requisição do tipo OPTIONS (preflight)
-    const xmr = page.waitForResponse(
-      (r) =>
-        r.request().url().includes("apimapas.inmet.gov.br/dados") &&
-        r.request().method() != "OPTIONS"
-    );
+    // Loop por cada parâmetro
 
-    //Esperar pela a resposta das requisiçõe ajax específicas
-    const ajaxResponse = await xmr;
+    for (const medition of stationParamsLabelsToQuery) {
+      //Selecionar medição
+      const selectMeditionTypeBtn = await page.$("#estacao-parametro");
+      await selectMeditionTypeBtn.select(medition);
 
-    const webResponse = await ajaxResponse.json();
+      const data = await page.$eval("#btn-estacao-BUSCAR", (el) => {
+        el.click();
+        return el.textContent.trim();
+      });
 
-    await setTimeout(10000);
+      console.log("Clicando no botão : ", data);
+
+      //Promise para esperar pela a resposta da requisição AJAX
+      //Que não seja a requisição do tipo OPTIONS (preflight)
+      const xmr = page.waitForResponse(
+        (r) =>
+          r.request().url().includes("apimapas.inmet.gov.br/dados") &&
+          r.request().method() != "OPTIONS"
+      );
+
+      //Esperar pela a resposta das requisiçõe ajax específicas
+      const ajaxResponse = await xmr;
+
+      const webResponse = await ajaxResponse.json();
+
+      console.log("Buscando dados da medição = ", medition);
+
+      if (webResponse) {
+        const { estacoes } = webResponse;
+
+        const stationsStream = dataAsStream(estacoes);
+
+        const readableStream = Readable.from(stationsStream);
+
+        const param = medition.split("-")[0];
+        // const writable = createWriteStream(
+        //   resolve(__dirname, "..", "data", "test.ndjson")
+        // );
+        const writable = new Writable({
+          objectMode: true,
+          write(chunk, enc, next) {
+            if (!datas.has(chunk.codigo)) {
+              datas.set(chunk.codigo, {
+                nome: chunk.nome,
+                estado: chunk.estado,
+                regiao: chunk.regiao,
+              });
+            }
+
+            datas.set(
+              chunk.codigo,
+              Object.assign(datas.get(chunk.codigo), {
+                [param]: chunk.valor,
+              })
+            );
+
+            next();
+          },
+        });
+
+        const sanitize = new Transform({
+          objectMode: true,
+          transform(chunk, enc, cb) {
+            const { codigo, nome, estado, regiao, latitude, longitude, valor } =
+              chunk;
+
+            const data = Object.assign(
+              {},
+              {
+                nome,
+                codigo,
+                estado,
+                regiao,
+                latitude,
+                longitude,
+                valor,
+              }
+            );
+
+            cb(null, data);
+          },
+        });
+
+        const filterByState = new Transform({
+          objectMode: true,
+          transform(chunk, enc, cb) {
+            const data = chunk;
+            if (state) {
+              if (state == data.estado) return cb(null, chunk);
+              else return cb(null);
+            } else {
+              cb(null, chunk);
+            }
+          },
+        });
+
+        await pipeline(readableStream, sanitize, filterByState, writable);
+      }
+    }
+
+    const stationDataToArray = [...datas.values()];
+
+    // await setTimeout(30000);
 
     await page.close();
 
     await browser.close();
 
-    if (webResponse) {
-      const { estacoes } = webResponse;
-
-      const stationsStream = dataAsStream(estacoes);
-
-      const readableStream = Readable.from(stationsStream);
-
-      const writable = createWriteStream(
-        resolve(__dirname, "..", "data", "test.ndjson")
-      );
-
-      readableStream
-        .pipe(
-          new Transform({
-            objectMode: true,
-            transform(chunk, enc, cb) {
-              const { nome, estado, regiao, latitude, longitude, valor } =
-                chunk;
-
-              const data = Object.assign(
-                {},
-                {
-                  nome,
-                  estado,
-                  regiao,
-                  latitude,
-                  longitude,
-                  valor,
-                }
-              );
-
-              cb(null, JSON.stringify(data).concat("\n"));
-            },
-          })
-        )
-        .pipe(writable);
-
+    if (stationDataToArray) {
       return res.status(200).json({
         message: `Sucesso ao obter dados da url ${urlPage}`,
-        data: estacoes,
+        data: stationDataToArray,
       });
     }
 
     return res.status(202).json({
-      message: "Não foi possível obter dados da estação",
+      message: "Não foi possível obter dados das estações",
     });
   } catch (error) {
     if (error instanceof Joi.ValidationError) {
