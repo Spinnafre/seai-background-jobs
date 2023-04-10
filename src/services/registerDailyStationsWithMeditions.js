e0de9d7165066ce7b594a1faef1fba459070369e
Spinnafre <davispenha@gmail.com>
2023-04-10 18:55:36 -0300

    chore: register daily station service

--- src/services/registerDailyStationsWithMeditions.js
+++ src/services/registerDailyStationsWithMeditions.js
@@ -1,1 +1,35 @@
-// Invoker
+import { InmetScrapper } from "../infra/scrapper/inmet-scrapper.js";
+
+class RegisterDailyStationsWithMeasures {
+  #inmetScrapper;
+
+  constructor(inmetScrapper) {
+    this.#inmetScrapper = inmetScrapper;
+  }
+
+  async execute(params) {
+    const inmetStations = await this.#inmetScrapper.getStations({
+      params,
+      timeout: 30000,
+    });
+
+    console.log(inmetStations);
+  }
+}
+
+// export { RegisterDailyStationsWithMeasures };
+
+const service = new RegisterDailyStationsWithMeasures(InmetScrapper);
+
+await service.execute({
+  country: "NE",
+  stations_type: "automaticas",
+  state: "CE",
+  date_type: "diario",
+  params: [
+    "Precipitação Total (mm)",
+    "Temp. Média (°C)",
+    "Umi. Média (%)",
+    "Vel. do Vento Média (m/s)",
+  ],
+});
