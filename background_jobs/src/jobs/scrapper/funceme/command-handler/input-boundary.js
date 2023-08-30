c5a597c40ae1e1c962f11636bab089efdde90cdf
Spinnafre <davispenha@gmail.com>
2023-08-29 23:14:08 -0300

    fix: funceme data miner DTO

--- background_jobs/src/jobs/scrapper/funceme/command-handler/input-boundary.js
+++ background_jobs/src/jobs/scrapper/funceme/command-handler/input-boundary.js
@@ -2,16 +2,16 @@ export class FuncemeDataMinerDTO {
   date;
 
   constructor(payload) {
-    this.date = payload;
-  }
-
-  // dd/mm/yyyy
-  getDate() {
     const separator = "-";
-    let month = this.date.getMonth() + 1;
-    let day = this.date.getDate();
+    let month = payload.getMonth() + 1;
+    let day = payload.getDate();
     if (month < 10) month = `0${month}`;
     if (day < 10) day = `0${day}`;
-    return `${this.date.getFullYear()}${separator}${month}${separator}${day}`;
+
+    this.date = `${payload.getFullYear()}${separator}${month}${separator}${day}`;
+  }
+
+  getDate() {
+    return this.date;
   }
 }
