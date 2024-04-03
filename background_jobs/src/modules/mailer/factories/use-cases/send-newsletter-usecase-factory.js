a78d8c29eee5115aa8a7310aa37703560679fcb1
Spinnafre <davispenha@gmail.com>
2024-04-03 17:10:43 -0300

    fix: mailer worker

--- background_jobs/src/modules/mailer/factories/use-cases/send-newsletter-usecase-factory.js
+++ background_jobs/src/modules/mailer/factories/use-cases/send-newsletter-usecase-factory.js
@@ -1,4 +1,4 @@
-import { DbNewsLetterRepository } from "../../../../shared/database/repositories/Newsletter.js";
+import { DbNewsLetterRepository } from "../../database/repositories/Newsletter.js";
 import { SendEmailService } from "../../external/send-email.js";
 import { SendNewsletterEmail } from "../../services/send-newsletter.js";
 
